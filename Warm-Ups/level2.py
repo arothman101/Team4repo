ec18c3afd41cd6fd3658a9239158e72053ead30e
arothman101 <45328262+arothman101@users.noreply.github.com>
2020-10-21 19:00:37 +0100

    Update level2.py

--- Warm-Ups/level2.py
+++ Warm-Ups/level2.py
@@ -1,12 +1,23 @@
-""" Hackathon - Level 2 """
+import string
 
-def longest_word(string):
-    # Add your solution here. You can use additional functions if need be.
-    # Don't forget to add a DocString for all your functions and comment your code.
-    # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    return "Your Return Value"
+array1 = ["qwgdg etfgdhfhgjfc sdfsdf sdfsdf. dfhfghsf sdfkhd.,fs."]
 
-if __name__ == '__main__':
-    # Add any code to test your solution here
-    # As per the example, this should return lorem
-    print(longest_word("lorem ipsum, dolor sit amet"))
+
+def longest_word(array1):
+  word = ""
+  list1 = []
+  for i in array1:
+    if i in list(string.punctuation) or i == " ":
+      list1.append(word)
+      word = ""
+    else:
+      word += i
+  longestword = ""
+  for i in list1:
+    if len(i) > len(longestword):
+      longestword = i
+
+
+  return longestword
+
+print(max(longest_word(array1[0])))
