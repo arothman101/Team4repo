04eef6c0b022daf3ce63b40ec2da9aa4133fad63
arothman101 <45328262+arothman101@users.noreply.github.com>
2020-10-21 19:01:06 +0100

    Update level3.py

--- Warm-Ups/level3.py
+++ Warm-Ups/level3.py
@@ -1,13 +1,11 @@
-""" Hackathon - Level 3 """
-
-def oddish_evenish(x):
-    # Add your solution here. You can use additional functions if need be.
-    # Don't forget to add a DocString for all your functions and comment your code.
-    # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    return "Your Return Value"
-
-if __name__ == '__main__':
-    # Add any code to test your solution here
-    # As per the example, this should return Oddish
-    print(oddish_evenish(1190))
+def evenish_oddish(num=str(input("Enter a number: "))):
+  summation = 0
+  for i in range(len(num)):
+    summation += int(num[i])
+  if summation % 2 == 0:
+    print("Evenish")
+  else:
+    print("Oddish")
+ 
+evenish_oddish()
     
