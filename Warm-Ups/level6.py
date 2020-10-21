0aaf02aa000de3fc1519252ca797e46d8d306af2
arothman101 <45328262+arothman101@users.noreply.github.com>
2020-10-21 19:02:12 +0100

    Update level6.py

--- Warm-Ups/level6.py
+++ Warm-Ups/level6.py
@@ -1,13 +1,15 @@
-""" Hackathon - Level 6 """
+def cipher(word, jumps):
+  encryption = ""
+  for char in word:
+    pos = ord(char)
+    for i in range(jumps):
+      pos += 1
+      if pos > 122:
+        pos -= 26
+    encryption += chr(pos)
+  return encryption
 
-def cipher(string, x):
-    # Add your solution here. You can use additional functions if need be.
-    # Don't forget to add a DocString for all your functions and comment your code.
-    # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    return "Your Return Value"
+word = str(input("Enter a word: "))
+jumps = int(input("Enter the number of jumps: "))
 
-if __name__ == '__main__':
-    # Add any code to test your solution here
-    # As per the example, this should return salve
-    print(cipher('pxisb', 3))
-    
+print(cipher(word.lower(), jumps))
